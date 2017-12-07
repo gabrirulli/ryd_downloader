4f079c29a6cc1bb32944116bdab380b369c85375
Gabriele Rulli <rulligabri.95@gmail.com>
2017-12-07 14:25:23 +0100

    Using backticks instead exec for execute youtube-dl command

--- methods.rb
+++ methods.rb
@@ -18,18 +18,32 @@ def ask_playlist_url_with_youtube_auth
   username = prompt("Username: ")
   password = prompt("Password: ")
   puts "\n"
-  exec "youtube-dl -u '#{username.chomp}' -p '#{password.chomp}' -o '%(playlist)s/%(title)s.%(ext)s' --download-archive playlist_ids.txt -x --audio-format mp3 '#{input.chomp}'"
+  `youtube-dl -u '#{username.chomp}' -p '#{password.chomp}' -o 'mp3-%(playlist)s/%(title)s.%(ext)s' -i --download-archive playlist_ids.txt -x --audio-format mp3 '#{input.chomp}'`
 end
 
 def ask_playlist_url
   input = prompt("Insert your playlist url (es: https://www.youtube.com/playlist\?list\=PLX9tyjZvM3XkulR7zLVChSubaqCNkYQRC)\n")
-  exec "youtube-dl -o '%(playlist)s/%(title)s.%(ext)s' --download-archive playlist_ids.txt -x --audio-format mp3 '#{input}'"
+  `youtube-dl -o 'mp3-%(playlist)s/%(title)s.%(ext)s' -i --download-archive playlist_ids.txt -x --audio-format mp3 '#{input}'`
 end
 
 ##### SINGLE AUDIO METHODS #####
 
+def uri_parser
+  begin
+    input = prompt("Insert your video url (es: https://www.youtube.com/watch?v=esWUomP7dgs)\n")
+    input = URI.parse(input).query.split("&").first.tr('v=','')
+  rescue URI::InvalidURIError => err
+    puts "Url format not valid\n".colorize(:red)
+    uri_parser
+  end
+end
+
 def ask_video_id
-  input = prompt("Insert your video url (es: https://www.youtube.com/watch?v=esWUomP7dgs)\n")
-  input = URI.parse(input).query.split("&").first.tr('v=','')
-  exec "youtube-dl -o '%(playlist)s/%(title)s.%(ext)s'-x --audio-format mp3 '#{input}'"
+  input = uri_parser
+  puts "Downloading..."
+  `youtube-dl -o 'mp3/%(title)s.%(ext)s' -i -x --audio-format mp3 '#{input}'`
+  if $?.success?
+    puts "Download successfull"
+  end
+  info_request
 end
